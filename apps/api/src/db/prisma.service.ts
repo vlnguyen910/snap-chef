1116be5a55c82921273b59bd311e146504d716b7
vlnguyen910 <volamnguyen447@gmail.com>
2025-12-03 15:42:40 +0700

    chore: connecting to db

---
+++ apps/api/src/db/prisma.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor() {
+    const adapter = new PrismaPg({ url: process.env.DATABASE_URL });
+    super({ adapter });
+  }
+}
