66be5a190e131a1ab2f72b04138236adf16a9543
vlnguyen910 <volamnguyen447@gmail.com>
2025-12-03 15:52:31 +0700

    refactor: import prisma service to app module and logging

--- apps/api/src/db/prisma.service.ts
+++ apps/api/src/db/prisma.service.ts
@@ -1,10 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
+
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
-    const adapter = new PrismaPg({ url: process.env.DATABASE_URL });
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    });
     super({ adapter });
+  }
+  async onModuleInit() {
+    await this.$connect();
+    console.log('Connected to the database');
   }
 }
