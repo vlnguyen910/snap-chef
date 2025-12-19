b7fec63d65de13a1fd9fd53480f86eb3b1a22267
nguyenvl910 <volamnguyen447@gmail.com>
2025-12-19 22:31:38 +0700

    feat: add update ingredient and step of recipe

--- apps/api/src/modules/recipes/recipes.service.ts
+++ apps/api/src/modules/recipes/recipes.service.ts
@@ -2,6 +2,8 @@ import {
   BadRequestException,
   Injectable,
   UnauthorizedException,
+  NotFoundException,
+  Logger
 } from '@nestjs/common';
 import { CreateRecipeDto } from './dto/create-recipe.dto';
 import { UpdateRecipeDto } from './dto/update-recipe.dto';
@@ -15,6 +17,8 @@ export class RecipesService {
     private prisma: PrismaService,
     private ingredientsService: IngredientsService,
   ) {}
+  
+  private readonly logger = new Logger(RecipesService.name);
 
   private validateOrderIndices(orderIndices: number[]): void {
     const sortedIndices = [...orderIndices].sort((a, b) => a - b);
@@ -110,10 +114,64 @@ export class RecipesService {
   //TODO: add update logic for ingredients and steps
   async update(id: number, updateRecipeDto: UpdateRecipeDto) {
     const { ingredients, steps, ...scalarFields } = updateRecipeDto;
+    
+    const oldRecipe = this.findOne(id);
+    if (!oldRecipe) throw new NotFoundException('Recipe not found');
 
-    return await this.prisma.recipe.update({
-      where: { id },
-      data: scalarFields,
-    });
+    return await this.prisma.$transaction(async (tx) => {
+      const updateRecipe = await tx.recipe.update({
+        where: { id },
+        data: scalarFields,
+      });
+
+      if (ingredients && ingredients.length > 0) {
+        this.logger.log(`Deleting ingredients of recipe: ${id}`);
+        await tx.recipeIngredient.deleteMany({
+          where: {recipe_id: id}
+        });
+
+        for (const item of ingredients) {
+          const ingredient = await this.ingredientsService.upsertByName(
+            item.name, 
+            tx
+          );
+
+          await tx.recipeIngredient.create({
+            data: {
+              recipe_id: id,
+              ingredient_id: ingredient.id,
+              quantity: item.quanity,
+              unit: item.unit,
+            },
+          });
+          this.logger.log(`An ingredient has been created`);
+        }
+      }
+
+      if (steps && steps.length > 0) {
+        this.logger.log(`Deleting steps of recipe: ${id}`);
+        await tx.step.deleteMany({
+          where: { recipe_id: id }
+        });
+
+        await tx.step.createMany({
+          data: steps.map((step) => ({
+            recipe_id: id,
+            order_index: step.order_index,
+            content: step.content,
+            image_url: step.image_url,
+          })),
+        });
+        this.logger.log(`${steps.length} steps have been created`);
+      }
+
+      return await tx.recipe.findUnique({
+        where: { id },
+        include: {
+          recipeIngredients: { include: { Ingredient: true } },
+          steps: { orderBy: { order_index: 'asc' } },
+        },
+      });
+    })
   }
 }
