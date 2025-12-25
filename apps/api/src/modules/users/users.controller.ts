25ba1ad5e972627e91d621285f2835946120e480
nguyenvl910 <volamnguyen447@gmail.com>
2025-12-25 20:31:25 +0700

    feat: add follow user endpoint

--- apps/api/src/modules/users/users.controller.ts
+++ apps/api/src/modules/users/users.controller.ts
@@ -6,9 +6,14 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import type { User } from 'src/generated/prisma/client';
+import { GetUser } from 'src/common/decorators/user.decorator';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 export class UsersController {
@@ -33,4 +38,13 @@ export class UsersController {
   // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
   //   return this.usersService.update(+id, updateUserDto);
   // }
+  
+  @Post(':id/follow')
+  @UseGuards(AuthGuard('jwt'))
+  followUser(
+    @GetUser() user: User,
+    @Param('id') following_id: string,
+  ) {
+    return this.usersService.followUser(user.id, following_id);
+  }
 }
