c3c76c85e8a4422f557a81b9b6fa4c0d0e8cd6da
Vo Lam Nguyen <volamnguyen447@gmail.com>
2026-01-16 14:15:22 +0700

    feat: add get user collections

--- apps/api/src/modules/collections/collection.service.ts
+++ apps/api/src/modules/collections/collection.service.ts
@@ -1,11 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/db/prisma.service';
 import { CreateCollectionDto } from './dto/create-collection';
 import { Colleciton } from 'src/generated/prisma/client';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class CollectionService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private userServicer: UsersService,
+  ) { }
 
   async create(
     user_id: string,
@@ -17,5 +21,26 @@ export class CollectionService {
         owner_id: user_id,
       },
     });
+  }
+
+  async getUserCollection(owner_id: string, current_user_id?: string | null) {
+    const owner = await this.userServicer.findOne(owner_id);
+    if (!owner)
+      throw new NotFoundException('User is not exist');
+    if (current_user_id) {
+      const currentUser = await this.userServicer.findOne(current_user_id);
+      if (!currentUser)
+        throw new NotFoundException('User is not exist');
+    }
+
+    const isOwner = owner_id === current_user_id;
+
+    return await this.prisma.colleciton.findMany({
+      where: {
+        owner_id,
+        ...(isOwner ? {} : { is_public: true }),
+      },
+      orderBy: { updated_at: 'desc' },
+    })
   }
 }
