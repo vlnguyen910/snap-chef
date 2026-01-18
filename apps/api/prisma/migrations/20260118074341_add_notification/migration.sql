734bd8f976ef17b99d04e2f70d26c0c18ad6118d
Vo Lam Nguyen <volamnguyen447@gmail.com>
2026-01-18 14:45:31 +0700

    chore: update recipe id to uuid and add notification at schema

---
+++ apps/api/prisma/migrations/20260118074341_add_notification/migration.sql
@@ -0,0 +1,90 @@
+/*
+  Warnings:
+
+  - The primary key for the `Like` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `Recipe` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `_CollectionToRecipe` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- CreateEnum
+CREATE TYPE "NotificationType" AS ENUM ('LIKE', 'COMMMENT', 'FOLLOW', 'SYSTEM');
+
+-- CreateEnum
+CREATE TYPE "NotificationResourceType" AS ENUM ('PROFILE', 'RECIPE', 'COLLECTION');
+
+-- DropForeignKey
+ALTER TABLE "Comment" DROP CONSTRAINT "Comment_recipe_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Like" DROP CONSTRAINT "Like_recipe_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "RecipeIngredient" DROP CONSTRAINT "RecipeIngredient_recipe_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Step" DROP CONSTRAINT "Step_recipe_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_CollectionToRecipe" DROP CONSTRAINT "_CollectionToRecipe_B_fkey";
+
+-- AlterTable
+ALTER TABLE "Comment" ALTER COLUMN "recipe_id" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "Like" DROP CONSTRAINT "Like_pkey",
+ALTER COLUMN "recipe_id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Like_pkey" PRIMARY KEY ("user_id", "recipe_id");
+
+-- AlterTable
+ALTER TABLE "Recipe" DROP CONSTRAINT "Recipe_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Recipe_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Recipe_id_seq";
+
+-- AlterTable
+ALTER TABLE "RecipeIngredient" ALTER COLUMN "recipe_id" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "Step" ALTER COLUMN "recipe_id" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "_CollectionToRecipe" DROP CONSTRAINT "_CollectionToRecipe_AB_pkey",
+ALTER COLUMN "B" SET DATA TYPE TEXT,
+ADD CONSTRAINT "_CollectionToRecipe_AB_pkey" PRIMARY KEY ("A", "B");
+
+-- CreateTable
+CREATE TABLE "Notification" (
+    "id" SERIAL NOT NULL,
+    "receiver_id" TEXT NOT NULL,
+    "sender_id" TEXT,
+    "type" "NotificationType" NOT NULL,
+    "message" TEXT NOT NULL,
+    "is_read" BOOLEAN NOT NULL DEFAULT false,
+    "resource_id" TEXT,
+    "resource_type" "NotificationResourceType",
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Notification_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "RecipeIngredient" ADD CONSTRAINT "RecipeIngredient_recipe_id_fkey" FOREIGN KEY ("recipe_id") REFERENCES "Recipe"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Step" ADD CONSTRAINT "Step_recipe_id_fkey" FOREIGN KEY ("recipe_id") REFERENCES "Recipe"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Like" ADD CONSTRAINT "Like_recipe_id_fkey" FOREIGN KEY ("recipe_id") REFERENCES "Recipe"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_recipe_id_fkey" FOREIGN KEY ("recipe_id") REFERENCES "Recipe"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Notification" ADD CONSTRAINT "Notification_receiver_id_fkey" FOREIGN KEY ("receiver_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Notification" ADD CONSTRAINT "Notification_sender_id_fkey" FOREIGN KEY ("sender_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CollectionToRecipe" ADD CONSTRAINT "_CollectionToRecipe_B_fkey" FOREIGN KEY ("B") REFERENCES "Recipe"("id") ON DELETE CASCADE ON UPDATE CASCADE;
