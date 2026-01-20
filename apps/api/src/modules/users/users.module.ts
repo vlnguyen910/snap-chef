3ef01018013c1491912fb2fd780877de7a09a699
Vo Lam Nguyen <volamnguyen447@gmail.com>
2026-01-20 15:14:22 +0700

    feat:  optimize recipe respone

--- apps/api/src/modules/users/users.module.ts
+++ apps/api/src/modules/users/users.module.ts
@@ -3,11 +3,16 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaModule } from 'src/common/db/prisma.module';
 import { RedisModule } from 'src/common/redis/redis.module';
+import { NotificationModule } from '../notifications/notification.module';
 
 @Module({
-  imports: [PrismaModule, RedisModule],
+  imports: [
+    PrismaModule,
+    RedisModule,
+    NotificationModule
+  ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
 })
-export class UsersModule {}
+export class UsersModule { }
