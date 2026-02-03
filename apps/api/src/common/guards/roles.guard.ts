d0c7021cb297e417ed6b752384f1eb8f5cd978c9
Vo Lam Nguyen <volamnguyen447@gmail.com>
2026-02-03 16:56:02 +0700

    refactor: update email verify service

--- apps/api/src/common/guards/roles.guard.ts
+++ apps/api/src/common/guards/roles.guard.ts
@@ -9,10 +9,10 @@ export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const requiredRoles = this.reflector.getAllAndOverride<UserRoles[]>(ROLES_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
+    const requiredRoles = this.reflector.getAllAndOverride<UserRoles[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
     if (!requiredRoles) {
       return true;
     }
