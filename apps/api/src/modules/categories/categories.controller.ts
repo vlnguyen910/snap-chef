0018cc5a9b405e2aac34dd91f35ee6ddefa32de5
Vo Lam Nguyen <volamnguyen447@gmail.com>
2026-03-05 10:19:11 +0700

    feat: add auth guard and role guard to controller

--- apps/api/src/modules/categories/categories.controller.ts
+++ apps/api/src/modules/categories/categories.controller.ts
@@ -1,24 +1,32 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, ParseIntPipe } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { JwtAuthGuard, RolesGuard } from 'src/common/guards';
+import { Roles } from 'src/common/decorators';
+import { UserRoles } from 'src/generated/prisma/enums';
+import { Category } from 'src/generated/prisma/client';
 
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) { }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRoles.ADMIN)
   @Post()
-  create(@Body() createCategoryDto: CreateCategoryDto) {
-    return this.categoriesService.create(createCategoryDto);
+  create(@Body() payload: CreateCategoryDto): Promise<Category> {
+    return this.categoriesService.create(payload);
   }
 
   @Get()
-  findAll() {
-    return this.categoriesService.findAll();
+  findAll(): Promise<Category[]> {
+    return this.categoriesService.findAll(true);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRoles.ADMIN)
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
-    return this.categoriesService.update(+id, updateCategoryDto);
+  update(@Param('id', ParseIntPipe) id: number, @Body() payload: UpdateCategoryDto): Promise<Category> {
+    return this.categoriesService.update(id, payload);
   }
 }
